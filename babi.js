9f8f2289e059d9d0fc29a1280891542ee934e94b
iboxyee <iboxyee@gmail.com>
2025-03-16 12:11:32 +0800

    Create babi.js

---
+++ babi.js
@@ -0,0 +1,5 @@
+function changeStyles() {
+          document.getElementById("sample").style.fontSize = "20px";
+          document.getElementById("sample").style.fontStyle = "italic";
+          document.getElementById("sample").style.color = "gold";
+}
